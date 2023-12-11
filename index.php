336d59ce3036d5371dd94fce2da3c7e06d394827
FedericoPiazzolla <Federicop257@gmail.com>
2023-12-11 17:22:37 +0100

    add: form in index.php

--- index.php
+++ index.php
@@ -5,7 +5,20 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>php-Badwords</title>
 </head>
+
 <body>
-  
+
+  <h1>Inserisci un paragrafo ed una parola da censurare</h1>
+
+  <form action="result.php" method="GET">
+
+    <label for="paragraph">Inserisci un paragrafo</label>
+    <input type="text" id="paragraph" name="">
+
+    <label for="censor">Inserisci una parola da censurare</label>
+    <input type="text" id="censor" name="">
+
+    <button type="submit">Invia</button>
+  </form>
 </body>
 </html>
